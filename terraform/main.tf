9e43c4bb70c6fe41df6ad28af52f1fce945e7fdf
Jaisurya R <88918258+RJaisurya@users.noreply.github.com>
2024-02-14 12:54:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
+    storage_account_name = "terraformgit47"
+    container_name       = "terraformfile1"
     key                  = "dev.terraform.tfstate"
   }
 }
